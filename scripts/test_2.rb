c73513c6209d12030a6e18e17c0e8767c8a9b3e9
beergeek <brett.gray@puppetlabs.com>
2016-08-02 14:26:43 +1000

    more tests

---
+++ scripts/test_2.rb
@@ -0,0 +1,76 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require 'puppet'
+require 'puppetclassify'
+
+# Have puppet parse its config so we can call its settings
+Puppet.initialize_settings
+
+class PuppetHttps
+  def get_with_token(url)
+    url = URI.parse(url)
+    accept = 'application/json'
+    token = File.read('/root/.puppetlabs/token')
+
+    req = Net::HTTP::Get.new("#{url.path}?#{url.query}", {"Accept" => accept, "X-Authentication" => token})
+    res = make_ssl_request(url, req)
+    res
+  end
+
+  def post_with_token(url, request_body=nil)
+    url = URI.parse(url)
+    token = File.read('/root/.puppetlabs/token')
+
+    request = Net::HTTP::Post.new(url.request_uri, {"X-Authentication" => token})
+    request.content_type = 'application/json'
+
+    unless request_body.nil?
+      request.body = request_body
+    end
+
+    res = make_ssl_request(url, request)
+    res
+  end
+end
+
+def load_config
+  master = Puppet.settings[:server]
+  @master = master
+  if master
+    @classifier_url   = "https://#{master}:4433/classifier-api"
+    @rbac_url         = "https://#{master}:4433/rbac-api"
+    @puppet_ca_url    = "https://#{master}:8140/puppet-ca"
+    @puppetdb_url     = "https://#{master}:8081/pdb"
+    @puppet_url       = "https://#{master}:8140/puppet"
+    @status_url       = "https://#{master}:8140/status"
+    @orchestrator_url = "https://#{master}:8143/orchestrator"
+    @activity_url     = "https://#{master}:4433/activity-api"
+    auth_info = {
+      'ca_certificate_path' => Puppet[:localcacert],
+      'certificate_path'    => Puppet[:hostcert],
+      'private_key_path'    => Puppet[:hostprivkey],
+    }
+    unless @api_setup
+      @api_setup = PuppetHttps.new(auth_info)
+    end
+  else
+    cputs "No master!"
+  end
+end
+
+def get_data(node_group_hash)
+  load_config
+  resource = JSON.parse(@api_setup.get("#{@classifier_url}/v1/groups").body)
+  node_groups.each do |node_group_data|
+    node_group_name.each do |resource_title|
+      resource = JSON.parse(@api_setup.get("#{@classifier_url}/v1/groups").body)
+      if resource.any?
+        puts "Success for #{node_name} for resource type #{resource_type.capitalize} and title #{resource_title}"
+      else
+        puts "Fail, cannot find resource type #{resource_type.capitalize} and title #{resource_title} for #{node_name}"
+      end
+    end
+  end
+end
+
+get_data()
