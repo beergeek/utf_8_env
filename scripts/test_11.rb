27eebcfbf18c5d960d4b39864125b48404620fd6
beergeek <brett.gray@puppetlabs.com>
2016-08-14 12:53:54 +1000

    more scripts

--- scripts/test_11.rb
+++ scripts/test_11.rb
@@ -1,9 +1,23 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 
 require 'puppet'
-require 'pry'
-require 'pry-byebug'
 require 'puppetclassify'
+
+@base_group_default = {
+  'role::base' => {
+    "utf_8_notify_string"   => "こんにちは",
+    "ensure_utf_8_concat"   => false,
+    "ensure_utf_8_registry" => false,
+    "ensure_utf_8_exported" => false,
+    "ensure_utf_8_virtual"  => false,
+    "ensure_utf_8_static"   => false,
+    "ensure_utf_8_group"    => false,
+    "ensure_utf_8_files"    => false,
+    "ensure_utf_8_nrp"      => false,
+    "ensure_utf_8_host"     => false,
+    "ensure_utf_8_users"    => false
+  }
+}
 
 @real_nodes = [
   'node0.puppet.vm',
@@ -40,6 +54,33 @@ Puppet.initialize_settings
 
 def cputs(string)
     puts "\033[1m#{string}\033[0m"
+end
+
+class PuppetHttps
+  def get_with_token(url)
+    url = URI.parse(url)
+    accept = 'application/json'
+    token = File.read('/root/.puppetlabs/token')
+
+    req = Net::HTTP::Get.new("#{url.path}?#{url.query}", {"Accept" => accept, "X-Authentication" => token})
+    res = make_ssl_request(url, req)
+    res
+  end
+
+  def post_with_token(url, request_body=nil)
+    url = URI.parse(url)
+    token = File.read('/root/.puppetlabs/token')
+
+    request = Net::HTTP::Post.new(url.request_uri, {"X-Authentication" => token})
+    request.content_type = 'application/json'
+
+    unless request_body.nil?
+      request.body = request_body
+    end
+
+    res = make_ssl_request(url, request)
+    res
+  end
 end
 
 # get Puppet master name
@@ -82,4 +123,23 @@ def get_data(node_names, resource_type, resource_titles)
   end
 end
 
+def update_master(mod_group, added_classes)
+  cputs "Updating #{mod_group} Node Group"
+  load_config
+  groups = JSON.parse(@api_setup.get_with_token("#{@classifier_url}/v1/groups").body)
+
+  node_group = groups.select { |group| group['name'] == mod_group}
+
+  raise "#{mod_group} group missing!" if node_group.empty?
+
+  group_hash = node_group.first.merge({"classes" => added_classes})
+  update_group = @api_setup.post_with_token("#{@classifier_url}/v1/groups/#{group_hash['id']}",group_hash.to_json)
+  if update_group.code.to_i != 200
+    cputs "Failed to update #{mod_group} #{update_group.code}"
+  else
+    cputs "Success in updating #{mod_group}"
+  end
+end
+
 get_data(@real_nodes, 'Group', @group_resources)
+update_master('ウェブ・グループ',@base_group_default)
